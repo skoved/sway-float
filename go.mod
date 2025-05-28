33efdb2861662622e1eb976878bb92730f09417a
skoved <skoved@redhat.com>
2025-05-28 14:39:46 -0400

    make windows float based on criteria

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/skoved/sway-float
 
 go 1.24.3
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/goccy/go-yaml v1.17.1
+	github.com/joshuarubin/go-sway v1.2.0
+)
+
+require (
+	github.com/joshuarubin/lifecycle v1.1.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,41 +4,96 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"slices"
+	"syscall"
 
-	"gopkg.in/yaml.v2"
+	"github.com/goccy/go-yaml"
+	"github.com/joshuarubin/go-sway"
 )
 
-const configDirSuffix = "/sway-float/config.yaml"
+const configDirSuffix = "sway-float/config.yaml"
 
 type floatConfig struct {
-	appId string `yaml:"app_id"`
-	mark  string `yaml:"con_mark"`
-	title string `yaml:"title"`
+	AppId string `yaml:"app_id"`
+	Mark  string `yaml:"con_mark"`
+	Title string `yaml:"title"`
 }
 
-// Prints err to stderr and calls os.Exit with statusCode
-func errorExit(err error, statusCode int) {
+func (c floatConfig) match(event sway.WindowEvent) bool {
+	return c.appIdMatch(event) && c.markMatch(event) && c.titleMatch(event)
+}
+
+// should only be called by match
+func (c floatConfig) appIdMatch(event sway.WindowEvent) bool {
+	if c.AppId == "" {
+		return true
+	}
+	if event.Container.AppID == nil {
+		return false
+	}
+	return c.AppId == *event.Container.AppID
+}
+
+func (c floatConfig) markMatch(event sway.WindowEvent) bool {
+	if c.Mark == "" {
+		return true
+	}
+	return slices.Contains(event.Container.Marks, c.Mark)
+}
+
+func (c floatConfig) titleMatch(event sway.WindowEvent) bool {
+	if c.Title == "" {
+		return true
+	}
+	return c.Title == event.Container.Name
+}
+
+// Prints err to stderr and calls os.Exit with a non-zero status code
+func errorExit(err error) {
 	fmt.Fprintln(os.Stderr, "Error:", err)
-	os.Exit(statusCode)
+	os.Exit(1)
 }
 
 func main() {
 	var conf floatConfig
 	configDir, confDirErr := os.UserConfigDir()
 	if confDirErr != nil {
-		errorExit(fmt.Errorf("could not determine the current user's config dir: %w", confDirErr), 1)
+		errorExit(fmt.Errorf("could not determine the current user's config dir: %w", confDirErr))
 	}
 	configPath := configDir + "/" + configDirSuffix
+	fmt.Fprintln(os.Stderr, "reading config from:", configPath)
 	configBytes, fileErr := os.ReadFile(configPath) //gosec:disable G304 -- need to build the path to the config dir
 	if fileErr != nil {
-		errorExit(fmt.Errorf("could not read file %s: %w", configPath, fileErr), 1)
+		errorExit(fmt.Errorf("could not read file %s: %w", configPath, fileErr))
 	}
 	yamlErr := yaml.Unmarshal(configBytes, &conf)
 	if yamlErr != nil {
-		errorExit(fmt.Errorf("could not parse yaml in file %s: %w", configPath, yamlErr), 1)
+		errorExit(fmt.Errorf("could not parse yaml in file %s: %w", configPath, yamlErr))
+	}
+	fmt.Println(conf.AppId, conf.Mark, conf.Title)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	handler, handlerErr := newWindowEventHandler(ctx, conf)
+	if handlerErr != nil {
+		errorExit(handlerErr)
 	}
 
-	fmt.Println(conf.appId, conf.mark, conf.title)
+	errCh := make(chan error)
+
+	go handler.handle(ctx, errCh)
+
+	sigCtx, sigCancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case subErr := <-errCh:
+		sigCancel()
+		errorExit(subErr)
+	case <-sigCtx.Done():
+		cancel()
+		fmt.Fprintln(os.Stderr, "Received interrupt. Shutting down")
+	}
 }
